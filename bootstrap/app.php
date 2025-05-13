7e0438432130118bca490014096aaf707299847f
dhasib <mdhasib522@gmail.com>
2025-05-13 09:33:08 +0600

    Add handling for ModelNotFoundException: Return JSON response for resource not found errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Auth\AuthenticationException;
 use App\Http\Middleware\ForceJsonRequestHeader;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Http\Middleware\RoleCheck;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -34,4 +35,11 @@ return Application::configure(basePath: dirname(__DIR__))
                 'message' => 'Not Authorized To Access This Resource. please login to continue',
             ], 401);
         });
+
+        $exceptions->render(function (ModelNotFoundException $e, Request $request) {
+            return response()->json([
+                'message' => 'Resource Not Found',
+            ], 404);
+        });
+
     })->create();
